8e38885e75891e6925c0694ff760acbacc236659
rogerice <rogerrodriguesti@gmail.com>
2023-03-01 17:08:08 -0300

    aplicação de envio de e-mail

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,38 @@
+package com.ms.email.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.ms.email.enums.StatusEmail;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel  implements Serializable{/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Long emailId;
+	private String ownerRef;
+	private String emailFrom;
+	private String emailTo;
+	private String subject;
+	
+	@Column(columnDefinition =  "TEXT")
+	private String text;
+	private LocalDate sendDateEmail;
+	private StatusEmail statusEmail;
+	
+	
+}
