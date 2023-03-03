d197d10289867e103b135012ddd3cb1866796936
rogerice <rogerrodriguesti@gmail.com>
2023-03-02 21:27:23 -0300

    Configurando fila Rabbitmq

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -2,6 +2,7 @@ package com.ms.email.models;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.UUID;
 
 import com.ms.email.enums.StatusEmail;
 
@@ -23,7 +24,7 @@ public class EmailModel  implements Serializable{/**
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
-	private Long emailId;
+	private UUID emailId;
 	private String ownerRef;
 	private String emailFrom;
 	private String emailTo;
